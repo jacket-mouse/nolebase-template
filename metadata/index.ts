a6df3a9593ab27b557f0fa8e3a704dc1839d8564
Yiming Li <109266207+jacket-mouse@users.noreply.github.com>
2024-07-12 12:56:01 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -9,6 +9,7 @@ export const include = ['笔记', '生活']
 /** Repo */
 export const githubRepoLink = 'https://github.com/jacket-mouse/self-learning'
 
+
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'nolebase.ayaka.io'
 /** 完整域名 */
